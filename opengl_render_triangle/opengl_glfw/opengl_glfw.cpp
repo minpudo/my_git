831018a55b970f7b456746e824412cb11a6eaa78
minpudo <pnt1614@naver.com>
2017-11-01 17:39:30 +0900

    Using shaders to render 2 triangles

---
+++ opengl_render_triangle/opengl_glfw/opengl_glfw.cpp
@@ -0,0 +1,167 @@
+
+#include "stdafx.h"
+#include <GL/glew.h>		
+#include <GLFW/glfw3.h> 
+#include <iostream>
+
+#include "LoadShaders.h"
+
+#define BUFFER_OFFSET(a) ((void*)(a))
+
+enum VAO_IDs { Triangles, NumVAOs };
+enum Buffer_IDs { ArrayBuffer, NumBuffers };
+enum Attrib_IDs { vPosition = 0 };
+
+GLuint  VAOs[NumVAOs];
+GLuint  Buffers[NumBuffers];
+
+const GLuint  NumVertices = 6;
+
+void window_resized(GLFWwindow* window, int width, int height);
+void key_pressed(GLFWwindow* window, int key, int scancode, int action, int mods);
+void show_glfw_error(int error, const char* description);
+
+
+void shader_and_buffer()
+{
+	glGenVertexArrays(NumVAOs, VAOs);
+	glBindVertexArray(VAOs[Triangles]);
+
+	GLfloat  vertices[NumVertices][2] = {
+		{ -0.90f, -0.90f },{ 0.85f, -0.90f },{ -0.90f,  0.85f },  // Triangle 1
+		{ 0.90f, -0.85f },{ 0.90f,  0.90f },{ -0.85f,  0.90f }   // Triangle 2
+	};
+
+	glCreateBuffers(NumBuffers, Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glBufferStorage(GL_ARRAY_BUFFER, sizeof(vertices), vertices, 0);
+
+	ShaderInfo  shaders[] =
+	{
+		{ GL_VERTEX_SHADER, "shaders/triangles/triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "shaders/triangles/triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);
+
+	glVertexAttribPointer(vPosition, 2, GL_FLOAT,
+		GL_FALSE, 0, BUFFER_OFFSET(0));
+	glEnableVertexAttribArray(vPosition);
+}
+
+void render(void)
+{
+	static const float black[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
+	glClearBufferfv(GL_COLOR, 0, black);
+
+	glBindVertexArray(VAOs[Triangles]);
+	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+}
+
+
+int main()
+{
+	// A call-back function for handling error
+	glfwSetErrorCallback(show_glfw_error);
+
+	/* Initialize the library */
+	if (!glfwInit()) {
+		std::cerr << "GLFW 초기화 실패" << '\n';
+		exit(-1);
+	}
+
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	/* Create a windowed mode window and its OpenGL context */
+	GLFWwindow* window = glfwCreateWindow(
+		800, // width
+		600, // height
+		"OpenGL Example",
+		NULL, NULL);
+	if (!window)
+	{
+		std::cerr << "Failed to create a window" << '\n';
+		glfwTerminate();
+		exit(-1);
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	// Call-back functions handling resizing windows & key events
+	glfwSetWindowSizeCallback(window, window_resized);
+	glfwSetKeyCallback(window, key_pressed);
+
+	glfwSwapInterval(1);
+
+	// glew
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (err != GLEW_OK) {
+		std::cerr << "GLEW 초기화 실패 " << glewGetErrorString(err) << '\n';
+		glfwTerminate();
+		exit(-1);
+	}
+
+	// output extensions
+	/*std::cout << glGetString(GL_VERSION) << '\n';
+
+	int nr_extensions = 0;
+	glGetIntegerv(GL_NUM_EXTENSIONS, &nr_extensions);
+
+	for (int i = 0; i < nr_extensions; ++i) {
+		std::cout << glGetStringi(GL_EXTENSIONS, i) << '\n';
+	}*/
+
+	shader_and_buffer();
+
+	glClearColor(0, 0, 1, 1);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+
+		// Clear buffer prior to rendering
+		//glClear(GL_COLOR_BUFFER_BIT);
+
+		render();
+
+		// Swap the front & the back buffers
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+
+	glfwDestroyWindow(window);
+
+
+	glfwTerminate();
+	return 0;
+}
+
+void show_glfw_error(int error, const char* description) {
+	std::cerr << "Error: " << description << '\n';
+}
+
+void window_resized(GLFWwindow* window, int width, int height) {
+	std::cout << "Window resized, new window size: " << width << " x " << height << '\n';
+
+	glClearColor(0, 0, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+}
+
+void key_pressed(GLFWwindow* window, int key, int scancode, int action, int mods) {
+	if ( (key == 'Q' || key == GLFW_KEY_ESCAPE) && action == GLFW_PRESS) {
+		glfwTerminate();
+		exit(0);
+	}
+}
